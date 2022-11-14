8e5bb6e099cd31d2bf37313b1c291a92f2632b25
LM <33723142+Lord-Mustang@users.noreply.github.com>
2022-11-14 16:28:30 +0100

    Initial commit

---
+++ test_filesystem/test_filesystem.cpp
@@ -0,0 +1,17 @@
+﻿// test_filesystem.cpp : Defines the entry point for the application.
+//
+
+#include "test_filesystem.h"
+
+#include <filesystem>
+#include <iostream>
+
+namespace fs = std::filesystem;
+
+int main()
+{
+	int i = 0;
+	fs::path path{ "../" };
+	std::cout << "Hello CMake." << std::endl;
+	return 0;
+}
